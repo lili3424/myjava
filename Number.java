65754ed40350239db8ff880d66e4c50550fbcbd5
lili3424 <58285418+lili3424@users.noreply.github.com>
2022-03-18 11:04:24 +0900

    .

---
+++ Number.java
@@ -0,0 +1,10 @@
+public class Number {
+public static void main(String[] args) {
+    byte mybyte = 124;
+    short myshort = 12344;
+    int myint = 24153521;
+    long mylong = 342342352L;
+    long result = mybyte + myshort + myint + mylong;
+    System.out.println("结果为:" + result);
+}
+}
